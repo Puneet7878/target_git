335d36c4c01a5df25209a0eb8c362a42c264ea5a
puneet_pw <puneet.mbt@gmail.com>
2017-07-27 09:12:31 +0530

    Build practice file

---
+++ Build-file-practice/src/hello/Buildrohini.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class Buildrohini {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Tiger on the hill");
+		System.out.println("Dragon below the hill");
+		
+
+	}
+
+}
