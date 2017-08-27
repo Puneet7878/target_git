4a6ee3abcadd25ac9330e5c1e88771476acd2a70
puneet_pw <puneet.mbt@gmail.com>
2017-08-28 00:26:16 +0530

    Jenkinsbuildtest

---
+++ JenkinsbuildTest/src/Proc/PracJenkins.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package Proc;
+
+import java.util.Scanner;
+
+/**
+ * @author admin
+ *
+ */
+public class PracJenkins {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		System.out.println(" You entered "+s);
+		
+		switch(s)
+		{
+			case "First":
+				System.out.println("This is the first step");
+				break;
+			case "Second":
+				System.out.println("This is the second step");
+				break;
+			case "Third":
+				System.out.println("This is the Third step");
+				break;
+			default:
+				System.out.println("No matching choice");
+		        break;
+		}
+		
+		
+		
+
+	}
+
+}
